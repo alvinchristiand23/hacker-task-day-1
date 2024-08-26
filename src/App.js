f80223f3355d42beaff89a580296f41832a9c38e
Alvin Christian Davidson <alvinchristiand23@gmail.com>
2024-08-26 23:02:46 +0800

    feat(src): input, add & remove card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Card from './components/Card';
+import { items } from './constant/data';
 
 function App() {
+  const [cards, setCards] = useState(items);
+  const [title, setTitle] = useState('');
+  const [subTitle, setSubTitle] = useState('');
+
+  const addItem = () => {
+    if (!title || !subTitle) {
+      return;
+    }
+
+    const newCard = {
+      title: title,
+      subTitle: subTitle,
+    };
+    setCards((e) => [...e, newCard]);
+    setTitle('');
+    setSubTitle('');
+  };
+
+  const removeItem = () => {
+    setCards((e) => e.slice(0, e.length - 1));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Task: Day 1 - React Component</h1>
+      <div className='input-container'>
+        <input
+          className='input'
+          placeholder='Title..'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          className='input'
+          placeholder='Sub Title..'
+          value={subTitle}
+          onChange={(e) => setSubTitle(e.target.value)}
+        />
+      </div>
+      <div className='button-container'>
+        <button className='button' onClick={addItem}>
+          Add Item
+        </button>
+        <button className='button' onClick={removeItem}>
+          Remove Item
+        </button>
+      </div>
+      <div className='card-container'>
+        {cards.map((item, index) => (
+          <Card key={item.title + index} title={item.title} subTitle={item.subTitle} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+export function Card({ title, subTitle }) {
+  return (
+    <div className='card'>
+      <h2>{title}</h2>
+      <h3>{subTitle}</h3>
+    </div>
+  );
+}
+
+export default Card;
